d2937820021410432b3daa3ab60be6f1e372c3f6
mkirisit <71440949+mkirisit@users.noreply.github.com>
2020-11-09 22:11:08 -0500

    Create main.c
    Created basic outline of main file

---
+++ main.c
@@ -0,0 +1,17 @@
+// Author: Michaela Kirisits
+// Purpose or Function: Automatic Kitty Feeder
+// --------------------------------------------------------------
+//Need to add include files and libraries as needed
+
+
+main ()
+{
+  //LCD screen Function
+  
+  //Servo Motor control function
+  
+  //Serving size calcualtor function
+  
+  //Pushbuttons function
+  
+}
