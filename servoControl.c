5eb75b5faa043dbc5918ec2a822cc4ec2056c7f4
mkirisit <71440949+mkirisit@users.noreply.github.com>
2020-11-09 22:12:41 -0500

    Create servoControl.c
    Create file for servo motor control

---
+++ servoControl.c
@@ -0,0 +1,11 @@
+// Author: Michaela Kirisits
+// Purpose or Function: Servo motor control
+// --------------------------------------------------------------
+//Need to add include files and libraries as needed
+
+
+servoControl ()
+{
+  //Servo Motor control function
+  
+}
